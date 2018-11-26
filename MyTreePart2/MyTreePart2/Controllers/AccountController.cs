7201884011f7280bd958b95d1b5d643d34f485aa
Jesper <jesp626a@edu.eal.dk>
2018-11-26 16:12:52 +0100

    Start

---
+++ MyTreePart2/MyTreePart2/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyTreePart2.Models.Account;
+
+namespace MyTreePart2.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            User user = new User
+            {
+                FirstName = "Karl",
+                LastName = "Erik",
+                Email = "KarlErikDenAnden@gmail.com"
+            };
+            return View(user);
+        }
+    }
+}
